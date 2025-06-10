bb81d93b3562ca78122688662259ee05c12ceecb
shindodesu <fjsndesu@gmail.com>
2025-06-10 16:30:24 +0900

    コミットメッセージ

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: {}, // <-- ここを修正
+    autoprefixer: {}, // autoprefixer も追加
+  },
 };
 
 export default config;
