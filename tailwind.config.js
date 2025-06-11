4ee9891845bbdc9b113697f4a394fc494b09a706
shindodesu <fjsndesu@gmail.com>
2025-06-11 16:49:49 +0900

    tailwind,configの中身修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', // src/pages 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
-    './src/components/**/*.{js,ts,jsx,tsx}', // src/components 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
-    // プロジェクト内で Tailwind CSS クラスを使用する他のディレクトリがあれば、ここに追加します。
+    './src/**/*.{js,ts,jsx,tsx}', // src/pages 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
   ],
   theme: {
     extend: {},
