0d0ab42319898374cf0c8430ac3cc8977a5e04f8
shindodesu <fjsndesu@gmail.com>
2025-06-11 18:13:54 +0900

    UIどうにかなんないかな

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', // src/pages 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
+    './src/pages/**/*.{js,ts,jsx,tsx}', // src/pages 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
+    './src/components/**/*.{js,ts,jsx,tsx}', // src/components 以下のすべてのJS/TS/JSX/TSXファイルをスキャン
+    // もし、src/ディレクトリ以外（例: public/index.htmlなど）でTailwindクラスを直接使用している場合は、ここに追加します。
   ],
   theme: {
     extend: {},
